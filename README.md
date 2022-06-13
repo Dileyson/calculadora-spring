0abb4bde22ee65f2008681711c2ef59d15aadb2d
dileyson <dem4.echeverria@gmail.com>
2022-06-13 11:03:23 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # calculadora-spring
+
+build the project with maven and do post request
+example request
+
+{
+"numero1":10,
+"numero2":2,
+"operacion":"+"
+}
---
+++ src/main/java/com/dem/springbootcalculadora/dto/CalculadoraResponse.java
@@ -0,0 +1,18 @@
+package com.dem.springbootcalculadora.dto;
+
+public class CalculadoraResponse {
+
+    private Double resultado;
+
+    public CalculadoraResponse() {
+    }
+
+    public Double getResultado() {
+        return resultado;
+    }
+
+    public void setResultado(Double resultado) {
+        this.resultado = resultado;
+    }
+
+}
